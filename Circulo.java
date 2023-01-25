10f4353103a4b127f88313247b4e9fcc5a4adfb8
EnriqueM11 <89398794+EnriqueM11@users.noreply.github.com>
2023-01-24 21:09:26 -0600

    Add files via upload

--- Circulo.java
+++ Circulo.java
@@ -1,5 +1,24 @@
 package circulo;
 
+
 public class Circulo {
+	public float id;
+	public float circunferencia;
+	public float area;
+	
+	public Circulo() {
+		this(174798);}
+	
+	public Circulo(float id) {
+	this.id=id;}
+	
+	public float circunferencia () {
+	return circunferencia=(float)(2*3.1416*id);
+	}	
+	
+	public float area() {
+		return area=(float) (3.1416*(Math.pow(id, 2)));
+		
+	}
 
 }
